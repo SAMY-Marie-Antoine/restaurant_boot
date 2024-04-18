603b7bd9fc7678a354ba266319e891f685d682f6
smarieantoine <smarieantoine@hotmail.com>
2024-04-18 15:06:50 +0200

    Création du projet resaturant_boot

---
+++ src/main/java/restaurant/RestaurantBootApplication.java
@@ -0,0 +1,13 @@
+package restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantBootApplication.class, args);
+	}
+
+}
