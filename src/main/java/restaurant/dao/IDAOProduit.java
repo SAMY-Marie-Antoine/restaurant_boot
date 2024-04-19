1860edacd7d21dd59c4a3e1081166067b5e9e490
hayet <hayett.makhlouf@gmail.com>
2024-04-19 16:01:03 +0200

    Produit modification MAJ

--- src/main/java/restaurant/dao/IDAOProduit.java
+++ src/main/java/restaurant/dao/IDAOProduit.java
@@ -3,10 +3,11 @@ package restaurant.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import restaurant.model.Produit;
 import restaurant.model.TypeProduit;
-
+@Component
 public interface IDAOProduit extends JpaRepository<Produit,Integer>{
 
     public List<Produit> findByType(TypeProduit type);
